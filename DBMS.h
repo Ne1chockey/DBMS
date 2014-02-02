76eab37c71a998a54962839413d7bcd157b51e3a
Gustavo Pedroso <gus@pedroso.com>
2014-02-02 17:42:39 -0600

    Added a .h file to start with

---
+++ DBMS.h
@@ -0,0 +1,39 @@
+/*******************************************************************************
+    File: DBMS.h
+
+    Authors: Gustavo Pedroso UIN: 423002834
+    		 Levi Clark UIN:
+    		 Terry Chen UIN:
+    		 Daniel He UIN:
+
+            Department of Computer Science
+            Texas A&M University
+    Date  : 2/2/2014
+
+    This file contains the header for the database management system
+    Formatting: * 80 pt width is used for code, for the most part
+				* Hungarian naming convention is used for variables
+				* Comments are applied for explanations
+				* Spacing and brackets are applied for readability
+*******************************************************************************/
+
+#ifndef DBMS_H
+#define DBMS_H
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class DBMS 
+{
+private:
+	//Declare private variables
+
+public:
+	//Declare class methods
+};
+
+#endif
