13e653df3927227df64b39cc18b2482d348d6827
Gustavo Pedroso <gus@pedroso.com>
2014-02-02 17:49:13 -0600

    Added .cpp for implementation of class and main.cpp file

---
+++ DBMS.cpp
@@ -0,0 +1,28 @@
+/*******************************************************************************
+    File: DBMS.cpp
+
+    Authors: Gustavo Pedroso UIN: 423002834
+    		 Levi Clark UIN:
+    		 Terry Chen UIN:
+    		 Daniel He UIN:
+
+            Department of Computer Science
+            Texas A&M University
+    Date  : 2/2/2014
+
+    Formatting: * 80 pt width is used for code, for the most part
+				* Hungarian naming convention is used for variables
+				* Comments are applied for explanations
+				* Spacing and brackets are applied for readability
+
+	This file contains the implementation for the database management system
+*******************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include "DBMS.h"
+using namespace std;
+
---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+    File: main.cpp
+
+    Authors: Gustavo Pedroso UIN: 423002834
+    		 Levi Clark UIN:
+    		 Terry Chen UIN:
+    		 Daniel He UIN:
+
+            Department of Computer Science
+            Texas A&M University
+    Date  : 2/2/2014
+
+    Formatting: * 80 pt width is used for code, for the most part
+				* Hungarian naming convention is used for variables
+				* Comments are applied for explanations
+				* Spacing and brackets are applied for readability
+
+	This file contains the main file 
+*******************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include "DBMS.h"
+using namespace std;
+
+int main 
+{
+	return 0;
+}
