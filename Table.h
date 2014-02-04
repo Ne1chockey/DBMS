fc82a3ccfbc7b564721fdd7bd6e16e4b5b37bb75
Gustavo Pedroso <gus@pedroso.com>
2014-02-04 14:16:44 -0600

    Update

---
+++ Table.h
@@ -0,0 +1,53 @@
+/*******************************************************************************
+    File: Table.h
+
+    Authors: Gustavo Pedroso UIN: 423002834
+             Levi Clark UIN:
+             Terry Chen UIN:
+             Daniel He UIN:
+
+            Department of Computer Science
+            Texas A&M University
+    Date  : 2/2/2014
+
+    Formatting: * 80 pt width is used for code, for the most part
+                * Hungarian naming convention is used for variables
+                * Comments are applied for explanations
+                * Spacing and brackets are applied for readability
+
+    This file contains the header for the table attributes
+*******************************************************************************/
+
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Table
+{
+private:
+    vector<string> vColumnName;
+    vector<tuple>> vRows;
+
+public:
+    void makeTuple();
+    Table()
+    {
+    };
+    void addColumn(string s){vColumnName.push_back(s);}
+    void addRow(tuple t){vRows.push_back(t);}
+
+};
+
+
+
+void makeTuple()
+{
+    ;
+}
+#endif
